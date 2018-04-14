65fc106e3216fcc1d5888641dc4d2da8c8e6e54d
Maulana Yusup Abdullah <adeneling@gmail.com>
2018-04-14 15:58:33 +0700

    Random Number when hit the role button

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceeIndex1 : Int  = 0;
+    var randomDiceeIndex2 : Int  = 0;
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnRollPressed(_ sender: UIButton) {
+        randomDiceeIndex1 = Int(arc4random_uniform(6));
+        randomDiceeIndex2 = Int(arc4random_uniform(6));
+        
+        print(randomDiceeIndex1);
+        print(randomDiceeIndex2);
+    }
 }
 
